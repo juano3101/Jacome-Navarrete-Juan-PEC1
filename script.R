eb5a519997e82b58db47ae6faf0b282cafdf3db6
Juan Carlos Jácome <juanjacome55@gmail.com>
2025-03-28 15:05:23 -0500

    se realizò gran parte de el analisis exploratorio

--- script.R
+++ script.R
@@ -2,86 +2,32 @@
 # https://www.metabolomicsworkbench.org/data/DRCCMetadata.php?Mode=Study&DataMode=FactorsData&StudyID=ST001322&StudyType=MS&ResultType=1#DataTabs
 # ST001322
 
+# datos
+# https://www.metabolomicsworkbench.org/data/DRCCMetadata.php?Mode=Study&DataMode=FactorsData&StudyID=ST001322&StudyType=MS&ResultType=1#DataTabs
+# ST001322
 
+#ion positive mod
 
 library(metabolomicsWorkbenchR)
-
-t(do_query('study','study_id','ST001322','summary'))
+t(do_query('study','study_id','ST000957','summary'))
 
 ### descargar datos
-
-SE <- do_query(
-      context = 'study',
-      input_item = 'analysis_id',
-      input_value = 'AN002198',
-      output_item = 'SummarizedExperiment'
-    )
-
-
-### como la estructura del query es diferente a la de los ejemplos
-### procedo a extraer los datos manuales y volver a crear un SummarizedExperiment
 library(SummarizedExperiment)
+library(readr)      # para leer TSV
+library(tibble)     # para asegurar nombres de filas
+library(S4Vectors)  # para DataFrame
 
-# Extraer la matriz de intensidades
-matriz <- as.data.frame(SE@assays@data@listData[[1]])
 
-# Extraer rowData y usar los nombres reales de metabolitos
-rowData <- as.data.frame(SE@elementMetadata)
-rownames(matriz) <- rowData$metabolite          # poner los nombres en la matriz
-rownames(rowData) <- rowData$metabolite         # poner los nombres en rowData
+# Cambia los nombres de archivo si es necesario
+assay <- read.delim("datos/assay.tsv", row.names = 1, check.names = FALSE)
+coldata <- read.delim("datos/coldata.txt", row.names = 1, check.names = FALSE)
+rowdata <- read.delim("datos/rowdata.tsv", row.names = 1, check.names = FALSE)
 
-# Extraer colData y alinear nombres
-colData <- as.data.frame(SE@colData)
-rownames(colData) <- colnames(matriz)           # los nombres de las muestras
+# Reordenar coldata para que coincida con el orden de las columnas de assay
+coldata <- coldata[colnames(assay), , drop = FALSE]
 
-# Crear el objeto SummarizedExperiment
 se <- SummarizedExperiment(
-  assays = list(abundancias = matriz),
-  rowData = rowData,
-  colData = colData
+  assays = list(count = assay),
+  colData = coldata,
+  rowData = rowdata
 )
-
-assay(se)
-
-# Eliminar metabolitos con nombre NA
-tail(rownames(se), 10)
-se <- se[!rownames(se) %in% c("NA", "NA.1"), ]
-
-
-# Convertir toda la matriz a números
-matriz <- assay(se)
-
-
-
-
-
-
-head(colData(se))
-
-str(colData(se))
-summary(colData(se))
-
-unique(se$treatment)
-unique(se$group)
-table(se$treatment)
-table(se$group)
-
-
-df <- as.data.frame(colData(se))
-df$tiempo <- gsub(".* (2 week|2 month|6 month).*", "\\1", df$treatment)
-
-colData(se)$tiempo <- df$tiempo
-
-
-
-library(ggplot2)
-df <- as.data.frame(colData(se))
-
-ggplot(df, aes(x = tiempo, fill = group)) +
-  geom_bar(position = "dodge") +
-  labs(title = "Distribución de muestras por grupo y tiempo",
-       x = "Tiempo", y = "Número de muestras") +
-  theme_minimal()
-
-
-
